63fa9523ff5eafaaa0e911146146606a441e70fc
oaz201 <oaz201@exeter.ac.uk>
2026-02-10 17:14:20 +0000

    Moved the django code in to the 4_code folder, added how to run the server locally to the README, linked the main, category and login pages

--- 4_code/djangoCode/djangoCode/asgi.py
+++ 4_code/djangoCode/djangoCode/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for failureMuseum project.
+ASGI config for djangoCode project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "failureMuseum.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoCode.settings")
 
 application = get_asgi_application()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Ai-Failure-Museum
 Teamwork project for University of exeter 2nd year.
+
+
+To run server locally do:
+source .venv/bin/activate
+cd 4_code/djangoCode
+python3 manage.py runserver
