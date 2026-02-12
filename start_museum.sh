21d9f9154f58821c7219a660615a0d5062d7510e
Lola120321 <101046252+Lola120321@users.noreply.github.com>
2026-02-12 15:52:21 +0000

    added .env.example
    added .env.example which should be used to create your own .env. Updated thr .sh and .bat and README to reflect this.

---
+++ start_museum.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "Starting the Museum"
+
+if [ ! -f .env]; then
+    echo -n "Enter a secure, random key to use as your museum key"
+    read user_key
+
+    echo "SECRET_KEY=$user_key" > .env
+    echo "DEBUG=True" >> .env
+else echo ".env already exists"
+fi 
+
+
+## checking for virtual enviroment, if not then create one
+if [! -d ".venv"]; then 
+    python3 -m venv .venv
+fi
+
+source .venv/bin/activate
+pip install -r requirements.txt
+## adding seeded data
+pyhton3 manage.py migrate 
+
+python3 manage.py loaddata seeded_data.json
+
+
+echo "Opening browser at http://127.0.0.1:8000/"
+
+python3 manage.py runserver
+
