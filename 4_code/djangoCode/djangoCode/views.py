34d46c51ee33d8d0be122f321f6dfbbf482f621f
Lola120321 <101046252+Lola120321@users.noreply.github.com>
2026-02-12 13:57:48 +0000

    updated views.py
    -added functionality for some curator tools like create, view analytics, delete, edit.
    - added comments to describe purpose, method and return for clarity (not completed yet)

--- 4_code/djangoCode/djangoCode/views.py
+++ 4_code/djangoCode/djangoCode/views.py
@@ -1,7 +1,8 @@
 from urllib import request
 from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from .forms import ArtefactForm
+from .models import Artefact
 
 def homepage(request):
     return render(request, "main.html")
@@ -27,29 +28,68 @@ def login(request):
 
 
 
-# to handle the upload of the artifact
-def upload_artefact(request):
-    
-  """   if request.method == "POST": 
-        form ArtefactForm(request.POST, request.FILES)
-        
-        if form.is_valid(): 
-            form.save()
-            return redirect('cureator_dashboard') #wehen successfully uploaded, redirect back to dashboard
-    else:
-        form = ArtefactForm() """
-  return render(request, 'curator/upload.html', {'form': form})
+# create_artefact 
+# Purpose: for the user to be able to create and upload an artefact
+# Method: get input for the fields 
+# Returns: redirect to curator dashboard
+
+
+# to handle the create and upload of the artifact
+def create_artefact(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Artefact.objects.create(title=title, description=description)
+        return redirect('curator_dashboard')
 
 
 
 
-# to handle the curator dashboard
+# curator_dashboard 
+# Purpose: provides a view for users (curators) to be able to view all artefacts that are active or archieved, be able to edit, archieve, delete or create new aratefacts, be able to (redirect) to view analystics.
+# Method: get a list of both unarchieved and archieved artefacts, then render the dashboard
+# Returns: render of the dashboard page with the list of artefacts
 
 def curator_dashboard(request):
     ##dashboard logic here
+    active_artefacts = Artefact.objects.filter(is_archived= False)
+    archived_aretfacts = Artefact.objects.filter(is_archived = True)
 
-    return render(request, 'curator/dashboard.html')
+    return render(request, 'curator/dashboard.html', {
+        'active_artefacts': active_artefacts,
+        'archived_artefacts': archived_aretfacts
+    })
 
+
+# delete_artefact 
+# Purpose: for user to be able to delete artefact, thus removing it from any list of artefacts and removing its data. 
+# Method: 1. get artefact. 2. delete 3. redirect
+# Returns: redirect to curator dashboard.
+
+def delete_artefact(request, pk): 
+    aretefact = get_object_or_404(Artefact, pk=pk)
+    artefact.delete()
+    return redirect('curator_dashboard') 
+
+# archieve_artefact 
+# Purpose: for user to be able to mark artefact as archieved, thus removing it from active list of artefacts that visitors see. 
+# Method: 1. get artefact. 2. seet is_archieved to true. 3. save, 4. redirect
+# Returns: redirect to curator dashboard.
+
+def archieve_artefact(request, pk):
+    artefact= get_object_or_404(Artefact, pk=pk)
+    artefact.is_archived = True
+    artefact.save()
+    return redirect('curator_dashboard')
+
+
+
+
+
+# analytics-_view 
+# Purpose: To provide user view of the analytics
+# Method: //
+# Returns: render the analytics page
 
 # to angle the analytics view/dashboard.
 def analytics_view(request):
