de29498977f4f39ecaf876aa2ea31a7d6e2dc5c0
Lola120321 <101046252+Lola120321@users.noreply.github.com>
2026-02-12 13:25:55 +0000

    curator views update
    - edited views.py to include a curator section, wher ei added the (empty) function for curator_dashboard and analytics_view.
    - Created a subfolder of templates called curator
    - Created placeholder html templates for analytics, upload, dashboard.

--- 4_code/djangoCode/djangoCode/views.py
+++ 4_code/djangoCode/djangoCode/views.py
@@ -1,3 +1,4 @@
+from urllib import request
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .forms import ArtefactForm
@@ -11,14 +12,46 @@ def category(request):
 def login(request):
     return render(request, "login.html")
 
+
+
+
+
+
+
+
+
+
+# ==========================================
+# CURATOR TOOLS 
+# ==========================================
+
+
+
 # to handle the upload of the artifact
 def upload_artefact(request):
-    if request.method == "POST": 
+    
+  """   if request.method == "POST": 
         form ArtefactForm(request.POST, request.FILES)
         
         if form.is_valid(): 
             form.save()
             return redirect('cureator_dashboard') #wehen successfully uploaded, redirect back to dashboard
     else:
-        form = ArtefactForm()
-    return render(request, 'upload.html', {'form' : form})
+        form = ArtefactForm() """
+  return render(request, 'curator/upload.html', {'form': form})
+
+
+
+
+# to handle the curator dashboard
+
+def curator_dashboard(request):
+    ##dashboard logic here
+
+    return render(request, 'curator/dashboard.html')
+
+
+# to angle the analytics view/dashboard.
+def analytics_view(request):
+    ## analystic dahsboard logic here
+    return render(request, 'curator/analytics.html')
