2bec296e0f0fa89bae91a8f222c729c5bb0843e7
Lola120321 <101046252+Lola120321@users.noreply.github.com>
2026-03-21 19:13:17 +0000

    Fixed bug
    Fixed, added user submission

--- 4_code/djangoCode/museum/forms.py
+++ 4_code/djangoCode/museum/forms.py
@@ -1,14 +1,14 @@
 from django import forms
-from .models import Exhibit, Quiz, Comment
+from .models import Exhibit, Quiz, Comment, UserSubmission
 
 class ExhibitForm(forms.ModelForm):
     class Meta:
         model = Exhibit
-        fields = ['title', 'category', 'failure_type', 'deployment_context', 
-        'intended_use', 'artefact1', 'system_type', 'inputs_assumptions', 
-        'outputs_to_users', 'artefact2', 'what_went_wrong', 'how_detected', 
-        'who_affected', 'artefact3', 'data_issues', 'design_choices', 
-        'org_governance_issues', 'artefact4', 'recommendations', 
+        fields = ['title', 'category', 'failure_type', 'deployment_context',
+        'intended_use', 'artefact1', 'system_type', 'inputs_assumptions',
+        'outputs_to_users', 'artefact2', 'what_went_wrong', 'how_detected',
+        'who_affected', 'artefact3', 'data_issues', 'design_choices',
+        'org_governance_issues', 'artefact4', 'recommendations',
         'warnings', 'artefact5', 'description', 'is_archived',]
         widgets = {
             'description': forms.Textarea(attrs={'rows': 3}),
@@ -20,6 +20,7 @@ class ExhibitForm(forms.ModelForm):
         }
 
 
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['body']
@@ -30,6 +31,18 @@ class ExhibitForm(forms.ModelForm):
             }),
         }
         labels = {'body': ''}
+
+
+class UserSubmissionForm(forms.ModelForm):
+    class Meta:
+        model = UserSubmission
+        fields = ['title', 'category', 'description', 'what_went_wrong',
+                  'who_affected', 'source_url', 'artefact']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 3}),
+            'what_went_wrong': forms.Textarea(attrs={'rows': 3}),
+            'who_affected': forms.Textarea(attrs={'rows': 2}),
+        }
 
 
 class QuizForm(forms.ModelForm):
